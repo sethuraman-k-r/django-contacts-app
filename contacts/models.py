3bbbe002020e45015336ccf61588c6572f6b4b0f
Sethuraman Krishnamoorthy <sethuraman.krishnamoorthy@tcs.com>
2019-11-24 13:55:16 +0530

    Templates added

--- contacts/models.py
+++ contacts/models.py
@@ -10,6 +10,9 @@ class User(models.Model):
     last_name = models.CharField(max_length=30, null=True)
     sur_name = models.CharField(max_length=30, null=True)
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 class Company(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     company_name = models.CharField(max_length=40, null=False)
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Hello World!!!</h1>")
+    print(request.session.keys())
+    template = loader.get_template('contacts/login.html')
+    return HttpResponse(template.render({},request))
